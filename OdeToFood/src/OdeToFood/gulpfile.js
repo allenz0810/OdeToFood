93c798d2425d900c06618ee17c4d6a5fc5f1feb5
allenz0810 <lingjiez321@gmail.com>
2016-10-27 22:19:13 -0400

    add gulp and set up envioment in layout page
    add gulp and set up envioment in layout page

---
+++ OdeToFood/src/OdeToFood/gulpfile.js
@@ -0,0 +1,6 @@
+﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+gulp.task('minify', function () {
+    return gulp.sec('node_modules/*').pipe(uglify()).pipe(gulp.dest('wwwroot/lib/_app'));
+});
