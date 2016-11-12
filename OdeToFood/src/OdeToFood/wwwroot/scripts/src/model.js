8e33856529d00ce0cc8c2615fbcbe7476ac77a50
allenz0810 <lingjiez321@gmail.com>
2016-11-12 14:16:19 -0500

    update
    update

---
+++ OdeToFood/src/OdeToFood/wwwroot/scripts/src/model.js
@@ -0,0 +1,9 @@
+"use strict";
+var HomeModel = (function () {
+    function HomeModel() {
+        this.compiler = "TypeScript";
+    }
+    return HomeModel;
+}());
+exports.HomeModel = HomeModel;
+//# sourceMappingURL=home.js.map
