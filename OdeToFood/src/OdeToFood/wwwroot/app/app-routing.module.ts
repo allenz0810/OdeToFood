f3900a2f8b0df3d7abe44603525e3cd8d2af6c7c
allenz0810 <lingjiez321@gmail.com>
2016-11-26 21:54:51 -0500

    mvoe source files
    mvoe source files

---
+++ OdeToFood/src/OdeToFood/wwwroot/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeIndexComponent } from './home/index.component';
+import { HomeCreateComponent } from './home/create.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home/index', pathMatch: 'full' },
+    { path: 'home/index', component: HomeIndexComponent },
+    { path: 'home/create', component: HomeCreateComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
