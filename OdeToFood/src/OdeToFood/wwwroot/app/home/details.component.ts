b838fea9507605b4175f3ea3f65af9004a04730e
allenz0810 <lingjiez321@gmail.com>
2016-12-15 19:53:01 -0500

    modify details to angular component
    modify details to angular component

---
+++ OdeToFood/src/OdeToFood/wwwroot/app/home/details.component.ts
@@ -0,0 +1,46 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Restaurant } from './restaurant';
+import { KeyValuePair } from './keyvaluepair';
+import { HomeService } from './home.service';
+
+@Component({
+    selector: 'home-create',
+    templateUrl: 'app/home/create.component.html'
+})
+export class HomeCreateComponent implements OnInit {
+
+    restaurant = new Restaurant();
+
+    Data: string = "This is a data coming from create";
+    cuisines: KeyValuePair<number>[] = [
+        { key: "None", value: 0 },
+        { key: "Italian", value: 1 },
+        { key: "French", value: 2 },
+        { key: "Japanese", value: 3 },
+        { key: "Amercian", value: 4 }
+    ];
+
+    constructor(
+        private homeService: HomeService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+    onSubmit() {
+        this.homeService.create(this.restaurant).then(() => this.goBack());
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+
+    add(name: string, cuisine: string): void {
+        alert("name: (" + name + "), cuisine: (" + cuisine + ")");
+    }
+}
